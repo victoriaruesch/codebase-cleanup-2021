79a153b8a999118cd51aeaaeb5ad37921b28a6f5
Victoria Ruesch <77935587+victoriaruesch@users.noreply.github.com>
2021-04-06 19:36:32 -0400

    Testing price formatting function (#2)
    Test our code.

--- app/shopping.py
+++ app/shopping.py
@@ -12,60 +12,66 @@ def format_usd(my_price):
     """
     return f"${my_price:,.2f}"
 
-# READ INVENTORY OF PRODUCTS
+# PREVENT ALL THE APP CODE FROM BEING IMPORTED
+# BUT STILL BE ABLE TO RUN IT FROM THE COMMAND LINE IS LIKE THIS...
 
-products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
-products_df = read_csv(products_filepath)
-products = products_df.to_dict("records")
-
-# CAPTURE PRODUCT SELECTIONS
-
-selected_products = []
-while True:
-    selected_id = input("Please select a product identifier: ")
-    if selected_id.upper() == "DONE":
-        break
-    else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        if any(matching_products):
-            selected_products.append(matching_products[0])
+if __name__ == "__main__":
+    
+    # READ INVENTORY OF PRODUCTS
+    
+    products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+    products_df = read_csv(products_filepath)
+    products = products_df.to_dict("records")
+    
+    # CAPTURE PRODUCT SELECTIONS
+    
+    selected_products = []
+    while True:
+        selected_id = input("Please select a product identifier: ")
+        if selected_id.upper() == "DONE":
+            break
         else:
-            print("OOPS, Couldn't find that product. Please try again.")
-
-checkout_at = datetime.now()
-
-subtotal = sum([float(p["price"]) for p in selected_products])
-
-# PRINT RECEIPT
-
-print("---------")
-print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
-print("---------")
-for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
-
-print("---------")
-print(f"SUBTOTAL: {subtotal:,.2f}")
-print(f"TAX: {(subtotal * 0.0875):.2f}")
-print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
-print("---------")
-print("THANK YOU! PLEASE COME AGAIN SOON!")
-print("---------")
-
-# WRITE RECEIPT TO FILE
-
-receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
-receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
-
-with open(receipt_filepath, "w") as receipt_file:
-    receipt_file.write("------------------------------------------")
+            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+            if any(matching_products):
+                selected_products.append(matching_products[0])
+            else:
+                print("OOPS, Couldn't find that product. Please try again.")
+    
+    checkout_at = datetime.now()
+    
+    subtotal = sum([float(p["price"]) for p in selected_products])
+    
+    # PRINT RECEIPT
+    
+    print("---------")
+    print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
+    print("---------")
     for p in selected_products:
-        receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
-
-    receipt_file.write("\n---------")
-    receipt_file.write(f"\nSUBTOTAL: {format_usd(subtotal)}")
-    receipt_file.write(f"\nTAX: {format_usd(subtotal * 0.875)}")
-    receipt_file.write(f"\nTOTAL: {format_usd(subtotal * 0.875 + subtotal)}")
-    receipt_file.write("\n---------")
-    receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
-    receipt_file.write("\n---------")
+        print("SELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
+    
+    print("---------")
+    print(f"SUBTOTAL: {subtotal:,.2f}")
+    print(f"TAX: {(subtotal * 0.0875):.2f}")
+    print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
+    print("---------")
+    print("THANK YOU! PLEASE COME AGAIN SOON!")
+    print("---------")
+    
+    # WRITE RECEIPT TO FILE
+    
+    receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
+    receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
+    
+    with open(receipt_filepath, "w") as receipt_file:
+        receipt_file.write("------------------------------------------")
+        for p in selected_products:
+            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+    
+        receipt_file.write("\n---------")
+        receipt_file.write(f"\nSUBTOTAL: {format_usd(subtotal)}")
+        receipt_file.write(f"\nTAX: {format_usd(subtotal * 0.875)}")
+        receipt_file.write(f"\nTOTAL: {format_usd(subtotal * 0.875 + subtotal)}")
+        receipt_file.write("\n---------")
+        receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
+        receipt_file.write("\n---------")
+    
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,8 @@
+# import the code we want to test
+from app.shopping import format_usd
 
-# TODO: import some code
 
-# TODO: test the code
+def test_format_usd():
+    #assert 2 == 4
+    #assert 2 == 2
+    assert format_usd(9.5) == "$9.50"
