b3bd5546ec8c8ebb50125d0fb7e17cf37e2e24ee
Victoria Ruesch <77935587+victoriaruesch@users.noreply.github.com>
2021-04-06 19:23:44 -0400

    documented the price formatting function

--- app/shopping.py
+++ app/shopping.py
@@ -3,6 +3,13 @@ from datetime import datetime
 from pandas import read_csv
 
 def format_usd(my_price):
+    """
+    Formats a number as USD with dollar sign and two decimals (and also thousands separator)
+
+    Params my_price is a number (int or float) that we want to format
+
+    Examples: format_usd(10)
+    """
     return f"${my_price:,.2f}"
 
 # READ INVENTORY OF PRODUCTS
